258c018db6de087165c30d30a7692bfcd253b1a0
versacegl <timehinsamuel11@gmail.com>
2024-07-24 17:47:22 +0300

    update

--- client/README.md
+++ client/README.md
@@ -1,3 +1,6 @@
+## Simple Chat App made with NextJS, NodeJS and Socket.io for Learning Purpose
+
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -5,28 +5,35 @@ import { io } from "socket.io-client";
 export default function Home() {
   const [socket,setSocket] = useState( undefined);
   const [index,setIndex] = useState(["hello","what's good"])
+  const [message,setMessage] = useState("")
 
   useEffect(()=>{
-    const socket = io("http://localhost:8080")
+    const socket = io("http://localhost:8080");
+
+    const handleMessage = ()=>{
+      const message =
+    }
+
+
   },[])
   return (
-    <div>
+    <div className="bg-white">
       <div className="flex flex-col gap-5 mt-20 px-10 lg:px-48">
       {/* Joining Room */}
       <div className="flex gap-2 items-center justify-center">
-              <input type="text" name="message" className="flex-1 bg-black border rounded px-2 py-1" />
-              <button className="w-40">Join room</button>
+              <input type="text" name="message" className="flex-1  border rounded px-2 py-1" />
+              <button className="w-40 bg-indigo-500 py-1">Join room</button>
           </div>
         {/* Showing the messages */}
-        <div className="flex flex-col gap-2 border rounded-lg p-10">
+        <div className="flex flex-col gap-2 border rounded-lg p-10 text-black">
           {index.map((message,index)=>(
             <div key={index} className="border rounded px-4 py-2">hello</div>
           ))}
         </div>
         {/* Send Message */}
           <div className="flex gap-2 items-center justify-center">
-              <input type="text" name="message" className="flex-1 bg-black border rounded px-2 py-1" />
-              <button className="w-40">Send Message</button>
+              <input type="text" name="message" className="flex-1  border rounded px-2 py-1" />
+              <button className="w-40 bg-indigo-500 py-1">Send Message</button>
           </div>
 
       </div>
