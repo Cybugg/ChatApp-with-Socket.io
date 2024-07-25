cc7454b6e00631e1036aadc5945245216acfa79b
versacegl <timehinsamuel11@gmail.com>
2024-07-25 17:12:41 +0300

    update fixes

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -10,6 +10,8 @@ export default function Home() {
 
   const handleMessage = ()=>{
     socket.emit("message", message, roomName)
+    setMessage("")
+    setRoomName("")
     };
   useEffect(()=>{
     const socket = io("http://localhost:8080"); 
@@ -31,23 +33,25 @@ export default function Home() {
   useEffect(()=>{})
 
   return (
-    <div className="bg-black text-black w-full flex items-center justify-center">
+    <div className="text-black w-full min-h-screen flex items-center justify-center" style={{
+     background:"URL('/static/background.png')"
+    }}>
       <div className="flex flex-col gap-5 mt-20 px-10 lg:px-48 w-96 items-center justify-center">
       {/* Joining Room */}
       <div className="flex gap-2 items-center justify-center">
-              <input type="text" name="room" className="flex-1  border rounded px-2 py-1" onChange={(e)=>setRoomName(e.target.value)} />
-              <button className="w-40 bg-indigo-500 py-1 text-white">Join room</button>
+              <input type="text" name="room" className="flex-1  border rounded px-2 py-1 outline-none" onChange={(e)=>setRoomName(e.target.value)} />
+              <button className="w-40 bg-green-600 py-1 text-white">Join room</button>
           </div>
         {/* Showing the messages */}
-        <div className="flex flex-col gap-2 border rounded-lg p-10 text-black w-96">
+        <div className="flex flex-col gap-2 border rounded-lg p-10 text-black w-96 bg-black bg-opacity-80">
           {inbox.map((message,index)=>(
             <div key={index} className="bubble left">{message}</div>
           ))}
         </div>
         {/* Send Message */}
           <div className="flex gap-2 items-center justify-center">
-              <input type="text" name="message" className="flex-1  border rounded px-2 py-1" onChange={(e)=>setMessage(e.target.value)} />
-              <button className="w-40 bg-indigo-500 py-1 text-white" onClick={handleMessage}>Send Message</button>
+              <input type="text" name="message" className="flex-1  border rounded px-2 py-1 outline-none" onChange={(e)=>setMessage(e.target.value)} />
+              <button className="w-40 bg-green-600 py-1 text-white" onClick={handleMessage}>Send Message</button>
           </div>
 
       </div>
